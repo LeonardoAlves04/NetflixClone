5cf3605422ac36602930a4da295144dd4be79484
Leonardo Alves <leonardoalves201510@gmail.com>
2023-01-30 16:38:22 -0300

    feat(): adding navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Row from './components/Row';
 import categories from './api';
 import Banner from './components/Banner';
+import Nav from './components/Nav'
 
 function App() {
   return (<div className="App">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 
+
 function Nav() {
+    const [show, setShow] = React.useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setShow(window.scroll > 100)
+        });
+        return () => {
+            window.removeEventListener()
+        };
+    })
     return (
-        <div>Nav</div>
+        <div className='navbar-container'>
+            <img className='nav-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158" alt='Netflix'></img>
+            <img className='nav-avatar' src='https://64.media.tumblr.com/76c9276332a286cbb7efd5c3a6efd1d1/tumblr_nusxze8qie1rpwm80o1_r1_250.png' alt='Leonardo'></img>
+        </div>
     )
 }
 
