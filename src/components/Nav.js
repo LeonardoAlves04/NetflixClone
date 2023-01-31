44b8c80a2385a394e6bf9f1083931e0d8f2f0107
Leonardo Alves <leonardoalves201510@gmail.com>
2023-01-31 14:07:36 -0300

    feat(): ending navbar (build, style, transitions ...)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,14 +7,12 @@ function Nav() {
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            setShow(window.scroll > 100)
+            setShow(window.scrollY > 100)
         });
-        return () => {
-            window.removeEventListener()
-        };
-    })
+    }, [])
+
     return (
-        <div className='navbar-container'>
+        <div className={`navbar-container ${show && "navbar-container-black"}`}>
             <img className='nav-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158" alt='Netflix'></img>
             <img className='nav-avatar' src='https://64.media.tumblr.com/76c9276332a286cbb7efd5c3a6efd1d1/tumblr_nusxze8qie1rpwm80o1_r1_250.png' alt='Leonardo'></img>
         </div>
